451ea278b87489970b9dacef5d0495e029dca676
Andreas Bourakis <bourakis@gmail.com>
2021-11-29 01:23:47 +0200

    Update ArrayExample1.cs

--- EXAMPLES/Arrays/ArrayExample1.cs
+++ EXAMPLES/Arrays/ArrayExample1.cs
@@ -1,28 +1,29 @@
 class ArrayExample1 
+{ 
+    public static void Main() 
     { 
-        public static void Main() 
+        Console.Write("Πόσες θερμοκρασίες θα δώσετε; "); 
+
+        int size = int.Parse(Console.ReadLine()); 
+        double[] temperatures = new double[size]; 
+
+        //Ανάγνωση των τιμών του πίνακα με ένα for 
+        for (int i=0; i < temperatures.Length; i++) 
         { 
-            Console.Write("Πόσες θερμοκρασίες θα δώσετε; "); 
-            
-            int size = int.Parse(Console.ReadLine()); 
-            double[] temperatures = new double[size]; 
-            
-            //Ανάγνωση των τιμών του πίνακα με ένα for 
-            for (int i=0; i < temperatures.Length; i++) 
-            { 
-                Console.Write("Δώσε την θερμοκρασία #{0}: ",i); 
-                temperatures[i] = double.Parse(Console.ReadLine()); 
-            } 
-            
-            //Υπολογισμός του μέσου όρου θερμοκρασίας με ένα foreach 
-            double sum = 0.0; 
-            foreach (double t in temperatures) 
-            { 
-                sum += t; 
-            } 
-            double average = sum / temperatures.Length; 
-            
-            Console.WriteLine("Η μέση θερμοκρασία είναι {0}",average); 
-            Console.ReadKey();  
+            Console.Write("Δώσε την θερμοκρασία #{0}: ", i); 
+            temperatures[i] = double.Parse(Console.ReadLine()); 
         } 
+
+        //Υπολογισμός του μέσου όρου θερμοκρασίας με ένα foreach 
+        double sum = 0.0; 
+        foreach (double t in temperatures) 
+        { 
+            sum += t; 
+        } 
+        
+        double average = sum / temperatures.Length; 
+
+        Console.WriteLine("Η μέση θερμοκρασία είναι {0}", average); 
+        Console.ReadKey();  
     } 
+} 
